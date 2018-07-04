89917c219ff7126991d5ff01408ddc9fb38daf81
karinakozarova <karinakozarova1303@gmail.com>
2018-07-04 14:28:15 +0300

    added registration to database

--- app/views.py
+++ app/views.py
@@ -2,6 +2,10 @@ from flask import Flask,render_template, redirect, url_for, request, flash
 from app import app
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 
+import sqlite3
+
+
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -50,19 +54,35 @@ class ReusableForm(Form):
  
 @app.route("/register", methods=['GET', 'POST'])
 def hello():
+
+
     form = ReusableForm(request.form)
  
     print form.errors
     if request.method == 'POST':
+        conn = sqlite3.connect("test.db")
+        c = conn.cursor()
+
         name=request.form['name']
         password=request.form['password']
         email=request.form['email']
         print name, " ", email, " ", password
- 
+
+
+
         if form.validate():
+            c.execute("INSERT INTO {} VALUES(?, ?, ?)".format("Users"), (name,email,password))
+            flash('Successfully registered! ' + name + password + email)
+            conn.commit()
+            conn.close()
+
             flash('Thanks for registration ' + name)
             return render_template("signed_in.html")
         else:
-            flash('Error: All the form fields are required. ')
- 
+            flash('Error: All the form fields are required.')
+            
+    conn.close()
     return render_template('register.html', form=form)
+
+
+
