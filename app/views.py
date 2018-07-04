2113eb5f5643a8edee81afa65e3e1aadfe84de3b
karinakozarova <karinakozarova1303@gmail.com>
2018-07-04 13:14:58 +0300

    added registration view

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from flask import Flask,render_template, redirect, url_for, request
+from flask import Flask,render_template, redirect, url_for, request, flash
 from app import app
-
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 
 @app.route('/')
 def index():
@@ -41,3 +41,28 @@ def login():
         else:
     		return render_template("signed_in.html")
     return render_template('login.html', error=error)
+
+
+class ReusableForm(Form):
+    name = TextField('Name:', validators=[validators.required()])
+    email = TextField('Email:', validators=[validators.required(), validators.Length(min=6, max=35)])
+    password = TextField('Password:', validators=[validators.required(), validators.Length(min=3, max=35)])
+ 
+@app.route("/register", methods=['GET', 'POST'])
+def hello():
+    form = ReusableForm(request.form)
+ 
+    print form.errors
+    if request.method == 'POST':
+        name=request.form['name']
+        password=request.form['password']
+        email=request.form['email']
+        print name, " ", email, " ", password
+ 
+        if form.validate():
+            flash('Thanks for registration ' + name)
+            return render_template("signed_in.html")
+        else:
+            flash('Error: All the form fields are required. ')
+ 
+    return render_template('register.html', form=form)
--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from app import app
 
 if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+    app.debug = True
     app.run()
